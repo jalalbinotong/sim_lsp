ef5d13a8e25e5f30a6b54b2b2873b041b21cb45e
AceVonLaura <66860122+AceVonLaura@users.noreply.github.com>
2023-05-14 16:55:55 +0800

    update 1.9 (dashboard fix)
    benerin dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,47 +1,50 @@
-<p class="title">
-    Dashboard
-    <?php echo $this->session->userdata('tipe_user'); ?>
-</p>
-<div class="row">
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card text-bg-warning">
-                <img src="https://cdn-icons-png.flaticon.com/512/4168/4168988.png" class="card-img-top" alt="...">
+<div class="col">
+    <p class="title">
+        Dashboard
+        <?php echo $this->session->userdata('tipe_user'); ?>
+    </p>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card text-bg-warning">
+                    <img src="https://cdn-icons-png.flaticon.com/512/4168/4168988.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Data Pegawai</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Kelola</a>
+                </div>
             </div>
-            <div class="card-body">
-                <h5 class="card-title">Data Pegawai</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Kelola</a>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card text-bg-success">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1415/1415345.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Data Schema</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Kelola</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card text-bg-primary">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2098/2098402.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Data Kegiatan</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                    <a href="#" class="btn btn-primary">Kelola</a>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card text-bg-success">
-                <img src="https://cdn-icons-png.flaticon.com/512/1415/1415345.png" class="card-img-top" alt="...">
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Data Schema</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Kelola</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card text-bg-primary">
-                <img src="https://cdn-icons-png.flaticon.com/512/2098/2098402.png" class="card-img-top" alt="...">
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">Data Kegiatan</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Kelola</a>
-            </div>
-        </div>
-    </div>
+
 </div>
 
 <style>
